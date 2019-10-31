d7ec78d3bb4d0b35b5b04107db8596180120377f
Derek Roberts <derek.roberts@kysu.edu>
2019-10-31 18:24:13 -0400

    created text field validation w/ dialog popup

--- app/src/main/java/com/example/panda/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/panda/ui/login/RegisterActivity.java
@@ -1,38 +1,45 @@
 package com.example.panda.ui.login;
 
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.panda.R;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
     private static final String ROOT_URL = "http://34.69.211.169/api/";
     private static final String URL_REGISTER = ROOT_URL + "users/create";
+
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
     private EditText usernameEditText;
     private EditText emailEditText;
     private EditText passwordEditText;
@@ -61,7 +68,6 @@ public class RegisterActivity extends AppCompatActivity {
         signUpButton = findViewById(R.id.signup);
         loadingProgressBar = findViewById(R.id.loading);
 
-
         // Here we create the spinner on the screen and give it the string array of classifications
         // from res/values/strings.xml to display upon clicking.
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -72,22 +78,105 @@ public class RegisterActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                registerUser(firstNameEditText.toString(), lastNameEditText.toString(),
-                        usernameEditText.toString(), emailEditText.toString(),
-                        passwordEditText.toString());
+                final Dialog dialog = new Dialog(RegisterActivity.this);
+                dialog.setContentView(R.layout.dialog);
+                dialog.setTitle("Input error.");
+                dialog.hide();
+
+                boolean isLegal = true;
+
+                username = usernameEditText.getText().toString();
+                email = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                firstName = firstNameEditText.getText().toString();
+                lastName = lastNameEditText.getText().toString();
+
+                TextView text = dialog.findViewById(R.id.text);
+
+                if(username.matches("")) {
+                    text.append("Username is empty");
+                    isLegal = false;
+                }
+
+                if(!isValidEmail(email)) {
+                    if(!isLegal) {
+                        text.append("\n");
+                    }
+                    text.append("Not a valid e-mail");
+                    isLegal = false;
+                }
+                if(!isValidPassword(password)) {
+                    if(!isLegal) {
+                        text.append("\n");
+                    }
+                    text.append("Not a valid password\n(At least 8 characters, One Number, One Uppercase)");
+                    isLegal = false;
+                }
+
+                if(firstName.matches("")) {
+                    if(!isLegal) {
+                        text.append("\n");
+                    }
+                    text.append("First name is empty");
+                    isLegal = false;
+                }
+
+                if(lastName.matches("")) {
+                    if(!isLegal) {
+                        text.append("\n");
+                    }
+                    text.append("Last name is empty");
+                    isLegal = false;
+                }
+
+                if(!isLegal) {
+                    Button dialogButton = dialog.findViewById(R.id.dialogButtonOK);
+                    dialog.show();
+                    dialogButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+                }
+
+                if(isLegal) {
+                    loadingProgressBar.setVisibility(View.VISIBLE);
+                    registerUser(firstName, lastName,
+                            username, email,
+                            password);
+                }
             }
         });
     }
 
+    private boolean isValidPassword(String password) {
+        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{8,}$";
 
+        Pattern pattern = Pattern.compile(passwordRegex);
+        if(password == null) {
+            return false;
+        }
+        return pattern.matcher(password).matches();
+    }
 
-    private void registerUser(final String firstName, final String lastName, final String username, final String email, final String password) {
-        /*
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        if (email == null) {
+            return false;
+        }
+        return pattern.matcher(email).matches();
+    }
+
+    private void registerUser(String firstName, String lastName, String username, String email, String password) {
+
         final String defaultBio = "Hi, I'm " + firstName + "!";
-        String cancelRequestTag = "register";
+        /*String cancelRequestTag = "register";
 
-        final JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, URL_REGISTER, null,
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_REGISTER, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -143,7 +232,47 @@ public class RegisterActivity extends AppCompatActivity {
 
         //System.out.println(params.toString() + "POOPASS");
         // Add request to queue.
-        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest, cancelRequestTag);*/
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest, cancelRequestTag);*/
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        Map<String, String> params = new LinkedHashMap<>();
+
+        params.put("username", username);
+        params.put("password", password);
+        params.put("email", email);
+        params.put("first_name", firstName);
+        params.put("last_name", lastName);
+        params.put("bio", defaultBio);
+        System.out.println(params.toString());
+
+        JSONObject userInput = new JSONObject(params);
+
+        System.out.println(userInput.toString());
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_REGISTER, userInput,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        if(response != null) {
+                            System.out.println("Response is: " + response.toString());
+                        } else {
+                            System.out.println("Response is null.");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
 
     private void showRegistrationFailed(@StringRes Integer errorString) {
