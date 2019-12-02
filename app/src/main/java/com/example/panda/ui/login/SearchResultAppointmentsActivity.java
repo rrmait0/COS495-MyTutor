798f7cd2ed815900827903cb9ecf70a2a66cdbec
Derek Roberts <derek.roberts@kysu.edu>
2019-12-02 13:39:33 -0500

    Empty Appointments activities and layouts + typo fix.

---
+++ app/src/main/java/com/example/panda/ui/login/SearchResultAppointmentsActivity.java
@@ -0,0 +1,16 @@
+package com.example.panda.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.panda.R;
+
+public class SearchResultAppointmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result_appointments);
+    }
+}
